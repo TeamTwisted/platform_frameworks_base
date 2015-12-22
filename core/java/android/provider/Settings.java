101ec3b2dd6e13400d46afa4060d8e00e186ba06
beanstown106 <nbenis106@gmail.com>
2015-12-22 02:13:26 -0500

    Double tap power button for camera - vibration toggle [1/2] added a toggle to disable double tap vibration when launching camera from the power button also lowered the vibration intensity from 3/4 of a second to 1/2 a second

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3595,6 +3595,12 @@ public final class Settings {
         public static final String DOZE_BRIGHTNESS = "doze_brightness";
 
          /**
+         * Enable vibration when double tapping the power button for camera
+         * @hide
+         */
+        public static final String DOUBLE_TAP_VIBRATE = "double_tap_vibrate";
+
+         /**
          * Enable double tap gesture anywhere on the lock screen put device to sleep
          * @hide
          */
