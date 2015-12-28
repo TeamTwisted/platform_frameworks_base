c98fed55e7f6bced0cddd97b789a0d399a4725da
LorDClocKAn <davor@losinj.com>
2015-12-28 16:50:28 -0500

    Fix up OnTheGo
    Change-Id: I9f3f61bbf7a36921e0ea103876196e9c174af219

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4122,13 +4122,6 @@ public final class Settings {
         public static final String POWER_MENU_ANIMATIONS = "power_menu_animations";
 
         /**
-         * If On-The-Go should be displayed at the power menu.
-         *
-         * @hide
-         */
-        public static final String POWER_MENU_ONTHEGO_ENABLED = "power_menu_onthego_enabled";
-
-        /**
          * The alpha value of the On-The-Go overlay.
          *
          * @hide
