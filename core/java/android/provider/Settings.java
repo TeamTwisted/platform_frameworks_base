37e8d2841f93f56f623221f41b06aa9b7861c0d5
beanstown106 <nbenis106@gmail.com>
2016-01-09 17:04:35 -0500

    Volume down in silent for dnd toggle *defaults to off *allows to be able to toggle wheter volume down in silent activates dnd

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4136,6 +4136,12 @@ public final class Settings {
         public static final String POWER_MENU_ANIMATIONS = "power_menu_animations";
 
         /**
+         * Boolean value whether volume down in silent can enter zen mode
+         * @hide
+         */
+        public static final String VOLUME_DOWN_ZEN = "volume_down_zen";
+
+        /**
          * The alpha value of the On-The-Go overlay.
          *
          * @hide
