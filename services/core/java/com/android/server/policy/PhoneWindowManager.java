191f540dbbd2ecede3dadf1bc4ce4eea3f397866
aow1980 <wilkes.allen@gmail.com>
2016-03-18 10:23:07 -0400

    base: readd custom rotation config support
    Change-Id: I8b82ad76052a61d50d617d54a0b29d186d35c27c

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -703,6 +703,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private final LogDecelerateInterpolator mLogDecelerateInterpolator
             = new LogDecelerateInterpolator(100, 0);
 
+    // constants for rotation bits
+    private static final int ROTATION_0_MODE = 1;
+    private static final int ROTATION_90_MODE = 2;
+    private static final int ROTATION_180_MODE = 4;
+    private static final int ROTATION_270_MODE = 8;
+
     private static final int MSG_ENABLE_POINTER_LOCATION = 1;
     private static final int MSG_DISABLE_POINTER_LOCATION = 2;
     private static final int MSG_DISPATCH_MEDIA_KEY_WITH_WAKE_LOCK = 3;
@@ -860,6 +866,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.NAVIGATION_BAR_SHOW), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.ACCELEROMETER_ROTATION_ANGLES), false, this,
                     UserHandle.USER_ALL);
             updateSettings();
         }
@@ -2121,8 +2130,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 updateOrientationListenerLp();
             }
 
-            mUserRotationAngles = Settings.System.getInt(resolver,
-                    Settings.System.ACCELEROMETER_ROTATION_ANGLES, -1);
+            mUserRotationAngles = Settings.System.getIntForUser(resolver,
+                    Settings.System.ACCELEROMETER_ROTATION_ANGLES, -1,
+                    UserHandle.USER_CURRENT);
 
             mNavigationBarCanMove = Settings.System.getIntForUser(resolver,
                     Settings.System.NAVIGATION_BAR_CAN_MOVE,
@@ -6930,25 +6940,25 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 }
                 // Rotation setting bitmask
                 // 1=0 2=90 4=180 8=270
-                boolean allowed = true;
                 if (mUserRotationAngles < 0) {
-                    // Not set by user so use these defaults
-                     mUserRotationAngles = mAllowAllRotations == 1 ?
-                            (1 | 2 | 4 | 8) : // All angles
-                                (1 | 2 | 8); // All except 180
+                    // defaults
+                    mUserRotationAngles = mAllowAllRotations == 1 ?
+                            (ROTATION_0_MODE | ROTATION_90_MODE | ROTATION_180_MODE | ROTATION_270_MODE) : // All angles
+                            (ROTATION_0_MODE | ROTATION_90_MODE | ROTATION_270_MODE); // All except 180
                 }
+                boolean allowed = true;
                 switch (sensorRotation) {
                     case Surface.ROTATION_0:
-                        allowed = (mUserRotationAngles & 1) != 0;
+                        allowed = (mUserRotationAngles & ROTATION_0_MODE) != 0;
                         break;
                     case Surface.ROTATION_90:
-                        allowed = (mUserRotationAngles & 2) != 0;
+                        allowed = (mUserRotationAngles & ROTATION_90_MODE) != 0;
                         break;
                     case Surface.ROTATION_180:
-                        allowed = (mUserRotationAngles & 4) != 0;
+                        allowed = (mUserRotationAngles & ROTATION_180_MODE) != 0;
                         break;
                     case Surface.ROTATION_270:
-                        allowed = (mUserRotationAngles & 8) != 0;
+                        allowed = (mUserRotationAngles & ROTATION_270_MODE) != 0;
                         break;
                 }
                 if (allowed) {
